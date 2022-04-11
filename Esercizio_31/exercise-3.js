e8c2fbc68196403d62a021213567c1cb6c858e5f
Andrea Rizzo <r.andrea2093@gmail.com>
2022-04-11 16:17:25 +0200

    Esercizi 28-33

---
+++ Esercizio_31/exercise-3.js
@@ -0,0 +1,23 @@
+function calculateAverageAge(persons) {
+  let personAge = persons.map(person => person.age)
+  let average = personAge.reduce((a, b) => (a + b), 0)
+  return average / personAge.length
+
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
