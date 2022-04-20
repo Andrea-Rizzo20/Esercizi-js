761a7908e820b5a34c91f2c3ce748a76f114e8d2
Andrea Rizzo <r.andrea2093@gmail.com>
2022-04-20 14:18:59 +0200

    fix Esercizio 42

--- Esercizio_42/exercise-2.js
+++ Esercizio_42/exercise-2.js
@@ -20,24 +20,38 @@ class BankAccount {
 
   withdraw(amount) {
   try{
-    if (amount < 0) {
-      throw new Error('The amount provided cannot be negative');
+    if(amount<0){
+      throw new Error('Questo campo non accetta valori negativi')
+    }else if(this.#amount < amount){
+      throw new Error('Non puoi prelevare una cifra superiore al bilancio corrente')
     }else{
-      try{
-        if(this.#amount < amount) {
-          throw new Error('You cannot withdraw more than account balance');
-        }else{
-          this.#amount -= amount;
-        }
-      }catch(err){
-        console.log('Non puoi prelevare una cifra superiore al bilancio corrente')
-      }
+      this.#amount -= amount;
     }
-
   }catch(err){
-    console.log('Questo campo non accetta valori negativi')
+    console.log(err)
   }
 }
+
+
+//     try{
+//     if (amount < 0) {
+//       throw new Error('The amount provided cannot be negative');
+//     }else{
+//       try{
+//         if(this.#amount < amount) {
+//           throw new Error('You cannot withdraw more than account balance');
+//         }else{
+//           this.#amount -= amount;
+//         }
+//       }catch(err){
+//         console.log('Non puoi prelevare una cifra superiore al bilancio corrente')
+//       }
+//     }
+
+//   }catch(err){
+//     console.log('Questo campo non accetta valori negativi')
+//   }
+// }
 
 
   view() {
